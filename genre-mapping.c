a34e62e1142f39708f08c6762dd3def8b12d8dcc
dvenkat <dhruv.venkat.2024@gmail.com>
2026-06-13 15:11:57 -0600

    program continuously runs in background to capture each song

--- genre-mapping.c
+++ genre-mapping.c
@@ -5,27 +5,26 @@
 
 void pullGenre() {
     FILE *fpipe;
-    char *command = "playerctl -p vlc metadata xesam:genre";
+    char *command = "playerctl --player=vlc --follow metadata xesam:genre";
     char* genre = NULL;
-    size_t capacity = 0;    
-
+    size_t capacity = 0;  
+    
     fpipe = (FILE*)popen(command, "r");
 
     if (fpipe == 0) {
         perror("popen() failed");
         exit(EXIT_FAILURE);
     }
+    
+    //ssize_t nread = ;
+    char *currGenre = genre;
 
-    ssize_t nread = getline(&genre, &capacity, fpipe);
-
-    if (nread == -1) {
-        perror("getline");
-        free(genre);
-        pclose(fpipe);
-        //return 1;
+    while (getline(&genre, &capacity, fpipe) != -1) {
+        printf("%s", genre);
     }
+    
 
-    printf("%s", genre);
-
+    perror("getline");
+    free(genre);
     pclose(fpipe);
 }
